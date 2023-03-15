346fadc1535ef06a676cf45bf8811de39fc7159f
Javier Iregui <iregui.javi@gmail.com>
2023-03-15 17:12:31 +0100

    Terminando estructura repositorio

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,21 @@
 const express = require('express');
 const router = express.Router()
 
+// Invocamos a UserAgent
+const useragent = require('express-useragent');
+router.use(useragent.express());
+
 // Invocamos a los controladores
 const deviceChecker = require('../controllers/deviceChecker')
+
+router.get('/', function(req, res){
+    if(!req.useragent.isDesktop){
+        res.send('MOVIL')
+    }
+    else{
+        res.send('DESKTOP')
+    }
+});
 
 // Rutas GET
 // router.get('/RUTA', FUNCIÓN, (req, res) => {
