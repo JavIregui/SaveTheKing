b808102b1aeeb4d04e07173e5432577a918d1e5e
Javier Iregui <iregui.javi@gmail.com>
2023-03-18 00:07:09 +0100

    Transiciones

--- public/js/transitionColor.js
+++
@@ -1,9 +0,0 @@
-// Get random color from list
-function setColor(){
-    const colors = ['#fdea18', '#53e200', '#008bd8', '#e42320', '#ef8216'];
-    const transition = document.getElementById("transition");
-    var color = colors[Math.round(Math.random() * 4)];
-    transition.style.backgroundColor = color;
-}
-
-window.onload = setColor;
