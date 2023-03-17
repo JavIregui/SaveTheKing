40759c10c98482085a5cf80b9b6e89b6f6eaa599
Javier Iregui <iregui.javi@gmail.com>
2023-03-17 23:35:41 +0100

    Añadiendo transición de carga

---
+++ public/js/transitionColor.js
@@ -0,0 +1,8 @@
+// Get random color from list
+function getColor(){
+    const colors = ['#fdea18', '#53e200', '#008bd8', '#e42320', '#ef8216'];
+    const transition = document.getElementById("transition");
+    var color = colors[Math.round(Math.random() * 4)];
+    transition.style.backgroundColor = color;
+}
+window.onload = getColor;
