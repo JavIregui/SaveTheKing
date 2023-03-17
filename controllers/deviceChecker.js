13b5071fda3221f57c878877dd5fa81553170480
Javier Iregui <iregui.javi@gmail.com>
2023-03-17 17:49:44 +0100

    Añadiendo Controlador de Dispositivo

--- controllers/deviceChecker.js
+++ controllers/deviceChecker.js
@@ -1,4 +1,13 @@
+// Invocamos a UserAgent
+const useragent = require('express-useragent');
+router.use(useragent.express());
+
 // checkDevice
-exports.checkDevice = (req, res) => {
-    
+exports.checkDevice = (req, res, next) => {
+    if(!req.useragent.isDesktop){
+        res.redirect('mobile')
+    }
+    else{
+        return next();
+    }
 }
--- routes/router.js
+++ routes/router.js
@@ -2,25 +2,16 @@
 const express = require('express');
 const router = express.Router()
 
-// Invocamos a UserAgent
-const useragent = require('express-useragent');
-router.use(useragent.express());
-
 // Invocamos a los controladores
 const deviceChecker = require('../controllers/deviceChecker')
 
-router.get('/', function(req, res){
-    if(!req.useragent.isDesktop){
-        res.redirect('/mobile')
-    }
-    else{
-        res.render('index')
-    }
+router.get('/', deviceChecker.checkDevice, (req, res) => {
+    res.render('index')
 });
-router.get('/lobby', function(req, res){
+router.get('/lobby', deviceChecker.checkDevice, (req, res) => {
     res.render('lobby')
 })
-router.get('/mobile', function(req, res){
+router.get('/mobile', (req, res) => {
     res.render('mobile')
 });
 
