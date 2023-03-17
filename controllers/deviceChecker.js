f86be68cd5dfee8444f924bbdd1326954e4d02ae
Javier Iregui <iregui.javi@gmail.com>
2023-03-17 17:56:00 +0100

    Creando Controlador de Dispositivo

--- controllers/deviceChecker.js
+++ controllers/deviceChecker.js
@@ -1,7 +1,3 @@
-// Invocamos a UserAgent
-const useragent = require('express-useragent');
-router.use(useragent.express());
-
 // checkDevice
 exports.checkDevice = (req, res, next) => {
     if(!req.useragent.isDesktop){
--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const router = express.Router()
 
+// Invocamos a UserAgent
+const useragent = require('express-useragent');
+router.use(useragent.express());
+
 // Invocamos a los controladores
 const deviceChecker = require('../controllers/deviceChecker')
 
@@ -15,15 +19,8 @@ router.get('/mobile', (req, res) => {
     res.render('mobile')
 });
 
-// Rutas GET
-// router.get('/RUTA', FUNCIÓN, (req, res) => {
-//     res.render('VIEW', {PARÁMETRO: VALOR})
-// })
-
-
 // Rutas POST
 //router.post('/RUTA', FUNCIÓN)
-
 
 // Exportamos el router
 module.exports = router;
