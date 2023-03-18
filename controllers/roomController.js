b15459247e11fa6bb0945dc977dde0310690cab6
Javier Iregui <iregui.javi@gmail.com>
2023-03-18 03:05:39 +0100

    Creando Controlador de Salas

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,3 +1,31 @@
+// Lista de Salas
+salas = [];
+
+// Generador de códigos
+var chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+function generateCode(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+// Crear sala
+exports.crearSala = (req, res) => {
+    roomIndex = salas.length;
+    salas.push({
+        code: generateCode(6,chars),
+        private: false,
+        canBeJoined: true,
+        head: req.ip,
+        members : [],
+        map: ''
+    });
+    salas[roomIndex].members.push(req.ip);
+}
+
+// Buscar sala disponible
+
+
 // Unirse a una sala
 exports.joinRoom = (req, res) => {
     res.send(req.ip)
